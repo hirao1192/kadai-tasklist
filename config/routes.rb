e74cf3de09c86ad55e65a8ac9fddae1f0ec474bf
sato.yuki <hirao11921@yahoo.co.jp>
2020-06-08 07:32:27 +0000

    post4

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
   
+  
+  
   resources :tasks, only: [:create, :destroy]
   
 end
