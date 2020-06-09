386f622d5f19965cec32462ad6841bdbcc10754d
sato.yuki <hirao11921@yahoo.co.jp>
2020-06-09 12:35:09 +0000

    post5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-  
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
     
     resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
