4861ec7825eefd6d1be906df51c65de4f1f73288
sato.yuki <hirao11921@yahoo.co.jp>
2020-06-10 12:57:50 +0000

    post10

--- db/migrate/20200530090321_create_tasks.rb
+++ db/migrate/20200530090321_create_tasks.rb
@@ -1,10 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :content
-      t.references :user, foreign_key: true
+    t.string :content
 
-      t.timestamps
+    t.timestamps
     end
   end
 end
